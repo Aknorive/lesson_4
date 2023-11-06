60ef1baa8215745a9588abb0cdcfba21510ed123
Aknorive <kazikova.veronika@gmail.com>
2023-11-06 18:47:37 +0100

    correction of homework

--- homework/ex1/script.js
+++ homework/ex1/script.js
@@ -142,45 +142,5 @@ const lastName = prompt('Zadejte sve prijmeni:')
   .normalize('NFKD')
   .replace(/[^\w]/g, '');
 
-
-document.body.innerHTML +=`<p>${lastName}${firstName}@fit.cvut.cz</p>`
-
-//ulozky na doma - Mnozstevni sleva
-/*const trickoPocet = Number(prompt('Zadejte pocet kusu: '));
-
-if (trickoPocet <= 49) {
-    document.body.innerHTML += '<p>Celkova cena objednavky: ' + (trickoPocet * 300) + ' czk</p>';
-} else if (trickoPocet >= 50 && trickoPocet <= 200) {
-    document.body.innerHTML += '<p>Celkova cena objednavky: ' + (trickoPocet * 250) + ' czk</p>';
-} else if (trickoPocet >= 201 && trickoPocet <= 500) {
-    document.body.innerHTML += '<p>Celkova cena objednavky: ' + (trickoPocet * 200) + ' czk</p>';
-} else if (trickoPocet >= 501 && trickoPocet <= 999) {
-    document.body.innerHTML += '<p>Celkova cena objednavky: ' + (trickoPocet * 150) + ' czk</p>';
-}
-else if (trickoPocet >= 1000) {
-    document.body.innerHTML += '<p>Celkova cena objednavky: ' + (trickoPocet * 120) + ' czk</p>';
-} else {
-    document.body.innerHTML += '<p>Neplatný počet kusů tricka</p>';
-}*/
-
-//Ulozky na doma - neide mi to !!!
-/*const Number = Number.prompt('Zadejte cislo od 0-36:');
-const sudaCisla = number % 2 === 0;
-const licheCisla = number % 2 !== 0;
-
-
-if (sudaCisla >= 0 && sudaCisla >= 28 ) {
-    document.body.innerHTML += '<p>Cerne cislo</p>';
-} else if (licheCisla >= 0 && licheCisla >= 28) {
-    document.body.innerHTML += '<p>Cervene cislo</p>';
-} else if (sudaCisla >= 29 && sudaCisla <= 36) {
-    document.body.innerHTML += '<p>Cervene cislo</p>';
-} else if (licheCisla >= 29 && licheCisla <= 36) {
-    document.body.innerHTML += '<p>Cerne cislo</p>';
-} else {
-    document.body.innerHTML += '<p>Neplatne cislo</p>';
-}*/
-
-
-
+document.body.innerHTML += `<p>${lastName}${firstName}@fit.cvut.cz</p>`;
 
