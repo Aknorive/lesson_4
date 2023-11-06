251dc5eefa0e621b896252cda7aae3131dab4e2b
Aknorive <kazikova.veronika@gmail.com>
2023-11-06 18:49:42 +0100

    correction of homework

--- homework/ex2/script.js
+++ homework/ex2/script.js
@@ -1,16 +1,20 @@
 const trickoPocet = Number(prompt('Zadejte pocet kusu: '));
 
 if (trickoPocet <= 49) {
-    document.body.innerHTML += '<p>Celkova cena objednavky: ' + (trickoPocet * 300) + ' czk</p>';
+  document.body.innerHTML +=
+    '<p>Celkova cena objednavky: ' + trickoPocet * 300 + ' czk</p>';
 } else if (trickoPocet >= 50 && trickoPocet <= 200) {
-    document.body.innerHTML += '<p>Celkova cena objednavky: ' + (trickoPocet * 250) + ' czk</p>';
+  document.body.innerHTML +=
+    '<p>Celkova cena objednavky: ' + trickoPocet * 250 + ' czk</p>';
 } else if (trickoPocet >= 201 && trickoPocet <= 500) {
-    document.body.innerHTML += '<p>Celkova cena objednavky: ' + (trickoPocet * 200) + ' czk</p>';
-} else if (trickoPocet >= 501 && trickoPocet <= 999) {
-    document.body.innerHTML += '<p>Celkova cena objednavky: ' + (trickoPocet * 150) + ' czk</p>';
-}
-else if (trickoPocet >= 1000) {
-    document.body.innerHTML += '<p>Celkova cena objednavky: ' + (trickoPocet * 120) + ' czk</p>';
+  document.body.innerHTML +=
+    '<p>Celkova cena objednavky: ' + trickoPocet * 200 + ' czk</p>';
+} else if (trickoPocet >= 501 && trickoPocet <= 1000) {
+  document.body.innerHTML +=
+    '<p>Celkova cena objednavky: ' + trickoPocet * 150 + ' czk</p>';
+} else if (trickoPocet >= 1001) {
+  document.body.innerHTML +=
+    '<p>Celkova cena objednavky: ' + trickoPocet * 120 + ' czk</p>';
 } else {
-    document.body.innerHTML += '<p>Neplatný počet kusů tricka</p>';
-};
+  document.body.innerHTML += '<p>Neplatný počet kusů tricka</p>';
+}
