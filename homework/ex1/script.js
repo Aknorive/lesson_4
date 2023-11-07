55b1265a25d7d45d06696c266aa61989b0ce7f0c
Aknorive <kazikova.veronika@gmail.com>
2023-11-07 14:55:05 +0100

    test

--- homework/ex1/script.js
+++ homework/ex1/script.js
@@ -1,7 +1,6 @@
-
-var str = 'áàâäãéèëêíìïîóòöôõúùüûñçăşţ';
-var asciiStr = str.normalize('NFKD').replace(/[^\w]/g, '');
-console.info(str, asciiStr);
+// var str = 'áàâäãéèëêíìïîóòöôõúùüûñçăşţ';
+// var asciiStr = str.normalize('NFKD').replace(/[^\w]/g, '');
+// console.info(str, asciiStr);
 
 const firstName = prompt('Zadejte sve krestni jmeno:')
   .trim()
@@ -16,5 +15,10 @@ const lastName = prompt('Zadejte sve prijmeni:')
   .normalize('NFKD')
   .replace(/[^\w]/g, '');
 
+let age = 15;
+age = 22;
+console.log(age);
+console.log(firstName);
 document.body.innerHTML += `<p>${lastName}${firstName}@fit.cvut.cz</p>`;
 
+console.log('hello');
